68de9e46239b0a79762ad34dae437297e553f26c
satyamsingh99777 <bhadoriya.satyam1507@gmail.com>
2024-07-11 13:37:34 +0530

    Update AppBar.tsx

--- hotel-reservation-app/hotel-reservation-frontend/src/layout/AppBar.tsx
+++ hotel-reservation-app/hotel-reservation-frontend/src/layout/AppBar.tsx
@@ -49,7 +49,11 @@ function UserMenu() {
         open={Boolean(anchorElUser)}
         onClose={handleCloseUserMenu}
       >
-        <MenuItem onClick={() => (window.location.pathname = "/reservations")}>
+        <MenuItem onClick={() => 
+sessionStorage.removeItem("userInfo");
+window.location.href =
+`/auth/logout?session_hint=${Cookies.get('session_hint')}`;
+}
           <Button style={{ textTransform: "none" }}>
             <Typography textAlign="center">My Reservations</Typography>
           </Button>
